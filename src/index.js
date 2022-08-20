7a9ae6429bcd9ea3aec30a4e7a84852e253f8919
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-20 18:54:28 -0500

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//ROOT ELEMENT from '../public/index.html' of the element with id="root"
+const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(rootElement)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<h1>Hello world</h1>)
