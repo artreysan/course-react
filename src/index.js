7609ddb8b946ebebacb93770c5ed5658ba0e5bda
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-20 19:06:41 -0500

    My first component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
+// My first component
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-//ROOT ELEMENT from '../public/index.html' of the element with id="root"
 const rootElement = document.getElementById('root')
 const root = ReactDOM.createRoot(rootElement)
 
-root.render(<h1>Hello world</h1>)
+//Creating component 'Greeting'
+function Greeting(){
+    return  <di>
+                <h1>Hello World</h1>
+                <p>lorem 3 </p>
+            </di> 
+}
+
+root.render(
+    <div>
+        {/*Call component method 1*/}
+        {Greeting()} 
+        {/*Call component method 2*/}
+        <Greeting></Greeting>
+    </div>
+)
