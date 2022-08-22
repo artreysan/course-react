cec11f0ecad118a63eea941a9ebf1a339e908d3a
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-22 00:04:10 -0500

    PROPS 3

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,14 +1,19 @@
 
-//Method 1
 export function Greeting(props) {
-    console.log(props)
     return <div>
         <h3>{props.title}</h3>
     </div>;
 }
 
-//Method 2
-export function UserCard({number,title = 'Default'} ){
-    console.log(number)
-    return <h1>Card {number}: {title} </h1>
+export function UserCard(props){
+    console.log(props.greet)
+    return <div>
+        <h1>{props.name}</h1>
+        <h1>{props.amount}</h1>
+        <h1>{props.married ? 'married': 'single'}</h1>
+        <ul>
+            <li>{props.address.city}</li>
+            <li>{props.address.street}</li>
+        </ul>
+    </div> 
 }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Greeting,UserCard} from './Greeting'
-import Product,{Navbar} from './Product'
+import { Greeting, UserCard } from "./Greeting";
+import Product, { Navbar } from "./Product";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
-
-root.render(<>
-    <UserCard number={11} title='Carta de amor'></UserCard>
-    <UserCard number={12} />
-    <Greeting title='Hola 1'/>
-    <Greeting title='Hola 2'/>
-    <Greeting title='Hola 3'/>
-    <Greeting title='Hola 4'/>
-    <Product></Product>
-    <Navbar></Navbar>
-</>);
+root.render(
+  <>
+    <UserCard
+      name="Ryan Ray"
+      amount={3000}
+      married={true}
+      point={[99, 33.4, 22.2]}
+      address={{ street: "123 Main Street", city: "New York" }}
+      greet={function(){alert('Hola Ryan')}}
+    ></UserCard>
+    <UserCard
+      name="Homer"
+      amount={5000}
+      married={false}
+      point={[99 ]}
+      address={{ street: "Allways Street", city: "CDMX" }}
+      greet={function(){alert('Hola Homer')}}
+    />  
+    </>
+);
