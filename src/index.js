bcbb07c174b5cb71071086d5bdcc556245f90b84
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-25 03:47:53 -0500

    Hooks: useState ==> Extra 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 // Hooks: useStage
-// https://reactjs.org/docs/hooks-reference.html#usestate
-
-//Import useState
 import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 
@@ -9,25 +6,13 @@ const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 function Counter(){
-  //useCase return a Arry, 1: variable and function
-  // variable is same to counter = 10
-  // function: change the state of variable 
-  const [counter, setCounter] = useState(10);
+  const [msj, setMessage] = useState('');
   
   return (
     <div>
-      <h1>Counter: {counter}</h1>
-      <button onClick={()=>{
-        setCounter(counter+1)
-      }}>Add</button>
-      <button onClick={()=>{
-        setCounter(counter-1)
-      }}>Sub</button>
-      <button onClick={()=>{
-        setCounter(0)
-      }}>Reset</button>
+      <input onChange={e => setMessage(e.target.value)}></input>
+      <button onClick={() => {alert('El mensaje es: '+msj)}}>Save</button>      
     </div>
-
   )
 }
 
