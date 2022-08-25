a9947f22d40c06670f8a73b0105db3267f4d0867
artreysan <70738181+beartree-ars@users.noreply.github.com>
2022-08-25 03:31:46 -0500

    Hooks: useState ==> Problem

--- src/index.js
+++ src/index.js
@@ -1,44 +1,30 @@
-// Hooks in React
-// https://es.reactjs.org/docs/hooks-intro.html
+// Hooks: useStage
+// https://reactjs.org/docs/hooks-reference.html#usestate
 
-import React from "react";
+//Import useState
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
-const users = [
-  {
-    id:1,
-    name:'Ryan',
-    image:'https://robohash.org/user1',
-  },
-  {
-    id:2,
-    name:'Joe',
-    image:'https://robohash.org/user2',
-  },
-]
+function Counter(){
+  let counter =0;
+
+  //No se actuala
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={()=>{
+         counter = counter+10;
+      }}>Add</button>
+    </div>
+  )
+}
+
 root.render(
   <>
-    {/*Recorrer arreglo user*/} 
-    {users.map((user)=>{return ' hola  '+user.name})};
 
-    {users.map(u => {return <h1>
-      Adios {u.name} 
-      </h1>
-    })};
-
-    {users.map((u,i)=> {return <h2 key={i}>
-      Hello {u.name} with  ID ={i+1}  
-      </h2>
-    })}
-    
-    {users.map((user,i)=> {return <div>
-        <h1 key ={i}>{user.name}</h1>
-        <img src={user.image}/>
-    </div>
-    })}
   </>
 );
 
